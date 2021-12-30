9287c8bd4db13a14d0e99d44e421dba261844961
Oscar Forss <oscar.forss.1@gmail.com>
2021-12-30 10:46:58 +0100

    Update movies

--- movie-app/src/main/java/io/wakelesstuna/moivefederationdemo/MovieService.java
+++ movie-app/src/main/java/io/wakelesstuna/moivefederationdemo/MovieService.java
@@ -11,11 +11,11 @@ import java.util.List;
 public class MovieService {
 
     private final List<Movie> movieList = List.of(
-            Movie.builder().id(1).title("Stranger Things").releaseYear(2016).build(),
-            Movie.builder().id(2).title("Ozark").releaseYear(2017).build(),
-            Movie.builder().id(3).title("The Crown").releaseYear(2016).build(),
-            Movie.builder().id(4).title("Dead to Me").releaseYear(2019).build(),
-            Movie.builder().id(5).title("Orange is the New Black").releaseYear(2013).build()
+            Movie.builder().id(1).title("The Lord of the Rings: The Fellowship of the Ring").releaseYear(2001).build(),
+            Movie.builder().id(2).title("Harry Potter and the Order of the Phoenix").releaseYear(2007).build(),
+            Movie.builder().id(3).title("The Avengers").releaseYear(2012).build(),
+            Movie.builder().id(4).title("Star Wars: Episode IV - A New Hope").releaseYear(1977).build(),
+            Movie.builder().id(5).title("Star Wars: Episode I – The Phantom Menace").releaseYear(1999).build()
     );
 
     public List<Movie> movies() {
